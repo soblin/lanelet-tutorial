5a214c646e3ec01f68a6b1083197799b31309cb2
soblin <hilo.soblin@gmail.com>
2022-07-14 09:55:47 +0900

    added src/05
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- src/05.cpp
+++ src/05.cpp
@@ -16,21 +16,29 @@ using namespace lanelet;
 using namespace std;
 
 void part1CreatingAndUsingRoutingGraphs(const LaneletMapPtr map);
-// void part2UsingRoutes(const LaneletMapPtr map);
+void part2UsingRoutes(const LaneletMapPtr map);
+void part2_1(const LaneletMapPtr map);
 // void part3UsingRoutingGraphContainers(const LaneletMapPtr map);
 
 int main() {
   // How to read lanelet2.osm
   string path =
       ament_index_cpp::get_package_share_directory("lanelet_tutorial");
-  path += "/mapping_example.osm";
+  string fpath = path + "/mapping_example.osm";
   lanelet::ErrorMessages errors{};
   lanelet::projection::MGRSProjector projector{};
-  lanelet::LaneletMapPtr map = lanelet::load(path, projector, &errors);
+  lanelet::LaneletMapPtr map = lanelet::load(fpath, projector, &errors);
   for (auto &&error : errors)
     cout << error << endl;
   part1CreatingAndUsingRoutingGraphs(map);
-  // part2UsingRoutes(map);
+  part2UsingRoutes(map);
+  fpath = path + "/kashiwanoha_intersection_area.osm";
+  lanelet::ErrorMessages errors2{};
+  lanelet::projection::MGRSProjector projector2{};
+  lanelet::LaneletMapPtr map2 = lanelet::load(fpath, projector2, &errors2);
+  for (auto &&error : errors2)
+    cout << error << endl;
+  part2_1(map2);
   // part3UsingRoutingGraphContainers(map);
 }
 
@@ -95,5 +103,55 @@ void part1CreatingAndUsingRoutingGraphs(const LaneletMapPtr map) {
     cout << way.id() << " ";
   cout << endl;
 }
-// void part2UsingRoutes(const LaneletMapPtr map);
+
+void part2UsingRoutes(const LaneletMapPtr map) {
+  traffic_rules::TrafficRulesPtr trafficRules =
+      traffic_rules::TrafficRulesFactory::create(Locations::Germany,
+                                                 Participants::Vehicle);
+  routing::RoutingGraphUPtr routingGraph =
+      routing::RoutingGraph::build(*map, *trafficRules);
+  ConstLanelet lanelet = map->laneletLayer.get(4984315);
+  ConstLanelet toLanelet = map->laneletLayer.get(2925017);
+
+  Optional<routing::Route> route =
+      routingGraph->getRoute(lanelet, toLanelet, 0);
+
+  routing::LaneletPath shortestPath = route->shortestPath();
+
+  LaneletSequence fullLane = route->fullLane(lanelet);
+  for (auto &&lane : fullLane)
+    cout << lane.id() << " ";
+  cout << endl;
+}
+
+void part2_1(const LaneletMapPtr map) {
+  traffic_rules::TrafficRulesPtr trafficRules =
+      traffic_rules::TrafficRulesFactory::create(Locations::Germany,
+                                                 Participants::Vehicle);
+  routing::RoutingGraphUPtr routingGraph =
+      routing::RoutingGraph::build(*map, *trafficRules);
+  ConstLanelet lanelet = map->laneletLayer.get(113);
+  ConstLanelet toLanelet = map->laneletLayer.get(134);
+  Optional<routing::Route> route =
+      routingGraph->getRoute(lanelet, toLanelet, 0);
+
+  routing::LaneletPath shortestPath = route->shortestPath();
+
+  LaneletSequence fullLane = route->fullLane(lanelet);
+  // 113 56 124 33 131 9041 30 134, which is as expeceted on VMB
+  for (auto &&lane : fullLane)
+    cout << lane.id() << " ";
+  cout << endl;
+
+  toLanelet = map->laneletLayer.get(112);
+  route = routingGraph->getRoute(lanelet, toLanelet, 0);
+  shortestPath = route->shortestPath();
+  fullLane = route->fullLane(lanelet);
+  // looks like the map is not well configured.
+  // 113 55 122 46 118 9061 97 9121 107 15 117 54 112
+  for (auto &&lane : fullLane)
+    cout << lane.id() << " ";
+  cout << endl;
+}
+
 // void part3UsingRoutingGraphContainers(const LaneletMapPtr map);
